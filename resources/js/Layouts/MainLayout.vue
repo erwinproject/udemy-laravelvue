c7467600f0075e4caad620c27814733fe8ac72fe
erwinproject <erwinproject.id@gmail.com>
2024-10-07 15:49:51 +0700

    auto commit [07/10/2024 15:49:51,16]

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,16 +4,32 @@
     <Link href="/listing">Listing Page</Link>&nbsp;
 
     <div>The page with time {{ timer }}</div>
+    
+    <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+    </div>
 
     <slot> Default </slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { Link, usePage } from '@inertiajs/vue3'
 
 // const timer = ref('cogs')
-
 const timer = ref(0)
 setInterval(() => timer.value++, 1000)
+
+// flash message
+const page = usePage()
+const flashSuccess = computed(
+  () => usePage().props.flash.success,
+)
 </script>
+
+<style scope>
+.success {
+    background-color: green;
+    color: white;
+}
+</style>
